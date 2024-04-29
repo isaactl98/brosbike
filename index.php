03966c18b43cadd17c0b0ea6cf825c30e2ca16ef
IsaacTorres <isaaccris1.01998@gmail.com>
2024-04-29 00:25:15 -0600

    DESARROLLO: SE ARREGLA PROBLEMA CON EL SIDEBAR

--- index.php
+++ index.php
@@ -34,9 +34,6 @@ require_once 'views/layout/head.php';
     /* ELIMINACION DE LA SESSSION */
     Utils::deleteSession('errorLogin');
     ?>
-    
-    <!-- SCRIPTS PERSONLALIZADOS -->
-    <script src="js/navbarstyle.js"></script>
     <!-- ./wrapper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
--- js/navbarstyle.js
+++ js/navbarstyle.js
@@ -1,10 +1,1 @@
 'use strict'
-$(document).ready(function () {
-  var navbar  =  document.getElementById("navbarshop");
-
-  if (window.innerWidth <= 600) {
-    navbar.style.display = "none";
-  } else {
-    navbar.style.display = "block";
-  }
-});
