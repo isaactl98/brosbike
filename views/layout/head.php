256c0d2626f87bcb9f41458ec5fd7e07871f9574
IsaacTorres <isaaccris1.01998@gmail.com>
2024-05-16 01:38:36 -0600

    SE CREA LA VISTA DE DETALLE DEL PRODUCTO

--- views/layout/head.php
+++ views/layout/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -18,5 +19,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@1&family=Syne:wght@700&display=swap" rel="stylesheet">
-    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?=baseUrl?>plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?=baseUrl?>dist/css/adminlte.min.css">
+
 </head>
